1ec8896451392f881edf9bdcd6c15a51e74e0f47
RobertKajnak <hezirisz@gmail.com>
2018-09-16 22:39:04 +0200

    Added a python script which does multiple runs and prints stats

---
+++ testSubmissionStats.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+
+TestSubmission
+Created on Sun Sep 16 20:56:01 2018
+
+@author: Hesiris
+"""
+if __name__ == "__main__":
+    import sys
+    
+    try:
+        if len(sys.argv)>=2:
+            runs_count=(int)(sys.argv[1])
+        else:
+            runs_count=38
+            
+        if len(sys.argv)>=3:
+            dirOffset = sys.argv[2]
+        else:
+            dirOffset = ''
+    except:
+        print("Usage: testSubmissionStats.py [nr_runs] [testrun.jar location] . Default value = 38 <current_dircetory> \n")
+        sys.exit()
+    
+    import subprocess
+    
+    #import os
+    #currpath = os.path.dirname(os.path.abspath(__file__))
+    resValTotal =0
+    maxVal = -1
+    minVal = 11
+    resRTTotal =0
+    maxRT = -1;
+    
+    for i in range(0,runs_count):
+        result = subprocess.run(["java","-jar","testrun.jar","-submission=player32","-evaluation=BentCigarFunction","-seed=1"],
+                             universal_newlines=True,stderr=subprocess.PIPE,shell=True,stdout=subprocess.PIPE)
+        
+        indScore = result.stdout.find("Score:");
+        resVal = (float)(result.stdout[indScore+7:indScore+13])
+        resValTotal += resVal
+        if resVal>maxVal:
+            maxVal=resVal
+        if resVal<minVal:
+            minVal=resVal
+        
+        
+        indRT = result.stdout.find("Runtime:");
+        indRTend = result.stdout.find("ms");
+        resRT = (float)(result.stdout[indRT+9:indRTend])
+        resRTTotal += resRT
+        if (resRT>maxRT):
+            maxRT=resRT
+        
+    print("Avg. Runtime = %f ; Worst run = %f"%(resRTTotal/runs_count,maxRT))
+    print("Avg. Result = %f; Minimum: %f; Maximum: %f"%(resValTotal/runs_count,minVal,maxVal))
+    
